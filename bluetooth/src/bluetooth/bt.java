4c70980d74147da8907ea1e07bd9b5eaf8727723
Jeremy Ruizenaar <jeremy.ruizenaar@student.hu.nl>
2016-03-23 16:38:25 +0100

    beetje bluetooth toch nxt

---
+++ bluetooth/src/bluetooth/bt.java
@@ -0,0 +1,19 @@
+package bluetooth;
+
+public class bt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		enableBT();
+		
+		
+		connectToNXTs();
+		
+		byte stuur 
+		
+		writeMessage(byte msg, nxt1);
+		
+		
+	}
+
+}
