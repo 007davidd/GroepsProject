b7d1cb0284f87fcd5fc3e18b7a59702bfd8cb346
Jeremy Ruizenaar <jeremy.ruizenaar@student.hu.nl>
2016-03-24 15:40:53 +0100

    ik heb geprobeerd mijn robot ontvangst code te uploaden ik hoop dat het lukt

--- blue/bluetooth/src/bluetooth/bt.java
+++ blue/bluetooth/src/bluetooth/bt.java
@@ -10,24 +10,21 @@ public class bt {
 		enableBT();				
 		connectToNXTs();
 		
-		byte [] motor = new byte[] {(byte)0x01,(byte)0x9B,(byte)0x00,(byte)0x00,(byte)0x80,(byte)0x00,(byte)0x00,(byte)0xA4,	
-							(byte)0x00,(byte)0x01,(byte)0x14,(byte)0xA6,(byte)0x00,(byte)0x01}; //motor aan   command
-		
-		
-		byte [] stuurmail = new byte[] {(byte)0x00,(byte)0x09,(byte)0x01,(byte)0x07,(byte)0x61,(byte)0x61,(byte)0x61,(byte)0x61,(byte)0x61,(byte)0x61,(byte)0x00  }; 
-		//verstuurt een string met 6 bytes naar mailbox 1 bevattende letter a
-		
-		
-		
+		byte [] stuur = new byte[] {(byte)0x01,(byte)0x9B,(byte)0x00,(byte)0x00,(byte)0x80,(byte)0x00,(byte)0x00,(byte)0xA4,	
+							(byte)0x00,(byte)0x01,(byte)0x14,(byte)0xA6,(byte)0x00,(byte)0x01};
+				
+				
 				
 		
-		for(int i = 0; i < 11; i++){ 				//does write message send stuur array in 1 x of moet er een for loop in 
-			writeMessage(stuurmail[i], "nxt1");
+
+		
+		for(int i = 0; i < 14; i++){
+			writeMessage(stuur[i], "nxt1");
 		
 		}
 		
-		//int ontvang = readMessage("nxt1");
-		//System.out.println(ontvang);
+		int ontvang = readMessage("nxt1");
+		System.out.println(ontvang);
 		
 		
 	}
