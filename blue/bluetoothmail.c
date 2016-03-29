06bd5da7674fd7779aebf17d5e1770d686bf152d
David Driessen <daviddriessen7@gmail.com>
2016-03-29 09:44:57 +0100

    Robotc test optimal

--- blue/bluetoothmail.c
+++ blue/bluetoothmail.c
@@ -1,6 +1,7 @@
 
 
 task main(){
+	int mailbox = 5;
 		int loopnum = 0;
 
 		//initialiseer display door alles leeg te maken
@@ -18,18 +19,17 @@ task main(){
 				nxtDisplayString(2, "loopnumber %d", loopnum);
 
 
-				// lees mailbox 1
-				if(cCmdMessageGetSize(1)){
-					  int a = cCmdMessageGetSize(1);          //haal de hoeveel bytes in eerst volgende bericht
-					  result = cCmdMessageRead(buffer, a, 1); // lees aantal bytes a in en plaats ze in buffer
-						ClearMessage();														// haal bericht uit de queue
-						nxtDisplayString(3, "result = %d", result);
+				// lees mailbox
+				int a = cCmdMessageGetSize(mailbox);  //haal de hoeveel bytes in eerst volgende bericht
+				if(a){        
+					result = cCmdMessageRead(buffer, a, mailbox); // lees aantal bytes a in en plaats ze in buffer
+					nxtDisplayString(3, "result = %d", result);
 
-						if(result)
-							message = buffer;
-									// stuur iets terug als we dat zouden willen
-									//sendMessage(3+10, message);
-						}
+					if(result)
+						message = buffer;
+							// stuur iets terug als we dat zouden willen
+							//sendMessage(3+10, message);
+				}
 
 				nxtDisplayString(4, "msg: %s", message);
 				wait1Msec(1000);
