1d3d258f91cce9ca4ad3dd58f6afb4aca2c5793f
Jeremy Ruizenaar <jeremy.ruizenaar@student.hu.nl>
2016-04-05 14:44:44 +0200

    + comments

--- findline-cirkel-way.c
+++ findline-cirkel-way.c
@@ -9,14 +9,13 @@
 void start(void);
 void Circle (int Lspeed, float Omtrek);
 
-int count = 0;
-int offset = 45;
-int Lspeed = 10;
-float Omtrek = 2500;
+int count = 0;                                      //bebug
+int offset = 45;																		// offset zwarte lijn
+int Lspeed = 10;																		//init speed linker wiel
+float Omtrek = 2500;																//init omtrek cirkel
 
 task rij(){
-
-			motor[MotorRechts] = 30;												//rij met een speed van 40 rechtdoor
+			motor[MotorRechts] = 30;												//rij met een speed van 30 rechtdoor
 			motor[MotorLinks] = 30;
 			while(true);
 }
@@ -25,25 +24,27 @@ task circle()
 {
 	while(true){
 
-		Circle(Lspeed,Omtrek);
-		Omtrek = Omtrek * 1.3;
-		if(Omtrek > 5000){
+		Circle(Lspeed,Omtrek);                          //aanroep functie die motor aanstuurt
+		Omtrek = Omtrek * 1.3;													// de omtrek van de cirkel word per ronde met 1,3 vermenigvuldigd
+		if(Omtrek > 5000){ 															// als deze grens overschreden word dan wordt de omtrek gelijk gemaakt aan een vaste waarde
 			Omtrek = 4700;
 		}
 
-		Lspeed = Lspeed + 10;
-		if(Lspeed > 35){
+		Lspeed = Lspeed + 10;       										//de snelheid van het linker wiel word wer per ronde met 10 verhoogd tot dat de grens van 35+ overschreden is
+		if(Lspeed > 35)
+		{
 			Lspeed = 35;
 		}
-		count++;
+		 count++;																				//debug
 
 	}
 
 }
 
-void start(void){
+void start(void)
+{
 	stopTask(circle);
-	startTask(rij);
+	startTask(rij);																//test functie stopcirkel start // auto
 }
 
 
@@ -51,12 +52,12 @@ void Circle(int Lspeed, float Omtrek)
 {
 
 	int en = nMotorEncoder[MotorRechts] + Omtrek;
-	motor[MotorRechts] = 50;													//speed moet varieren om langere of kortere bocht te krijgen
-	motor[MotorLinks] = Lspeed;
+	motor[MotorRechts] = 50;
+	motor[MotorLinks] = Lspeed;												//speed linker wiel moet varieren om langere of kortere bocht te krijgen
 	while(nMotorEncoder[MotorRechts] < en)
 	{
-		if(SensorValue[LichtL] < offset)
-		{
+		if(SensorValue[LichtL] < offset)            //if tijdens het rond rijden en zoeken naar de lijn de lijn gevonden word stop dan alles en start de task rij auto
+		{																						// aangezien de sensor op de lijn zit kan de auto functie direct overnemen
 			playSound(soundBeepBeep);
 			motor[MotorRechts] = 0;
 			motor[MotorLinks] = 0;
