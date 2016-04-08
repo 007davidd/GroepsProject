9790dbeaf0ec54b8e817143ad2f764bee35fbbc2
David Driessen <daviddriessen@live.nl>
2016-04-08 15:09:28 +0200

    Code van app toegevoegd.

---
+++ BluetoothRemote-master/app/src/main/java/nl/hu/zrb/btclient/ConnectActivity.java
@@ -0,0 +1,146 @@
+package nl.hu.zrb.btclient;
+
+import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.Set;
+
+public class ConnectActivity extends ListActivity  {
+	
+	DeviceListAdapter adapter;
+	BluetoothAdapter btadapter = BluetoothAdapter.getDefaultAdapter();
+    Button b;
+    String tag = "BluetoothRemoteControl";
+    int REQUEST_ENABLE_BT = 234;
+
+
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (btadapter == null) {
+            /*String text = "Your device does not support bluetooth.";
+            Toast toast = Toast.makeText(this, text, Toast.LENGTH_LONG);
+            toast.show();*/
+            ListView lv = getListView();
+            TextView tv = new TextView(this);
+            tv.setText("Your device does not support bluetooth");
+            ((ViewGroup)lv.getParent()).addView(tv);
+            lv.setEmptyView(tv);
+            return;
+        }
+        Log.i(tag, "oncreate");
+        adapter = new DeviceListAdapter(this);
+        ListView lv = getListView();
+        TextView tv = new TextView(this);
+        tv.setText("no devices");
+        ((ViewGroup)lv.getParent()).addView(tv);
+        lv.setEmptyView(tv);
+
+        if(!btadapter.isEnabled()){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+        else {
+            Set<BluetoothDevice> pairedDevices = btadapter.getBondedDevices();
+            for(BluetoothDevice dev: pairedDevices)
+                adapter.add(dev);
+        }
+        setListAdapter(adapter);   
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_connect, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.refresh && btadapter != null){
+            adapter.clear();
+            onContentChanged();
+            boolean succes = btadapter.startDiscovery();
+            if(succes)
+                Log.i(tag, "discovery started");
+            else
+                Log.i(tag, "discovery not started");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onStart(){
+        super.onStart();
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+    }
+
+    public void onStop(){
+        super.onStop();
+        unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent i ){
+        if(resultCode == RESULT_OK && requestCode == REQUEST_ENABLE_BT){
+            Set<BluetoothDevice> pairedDevices = btadapter.getBondedDevices();
+            for(BluetoothDevice dev: pairedDevices)
+                adapter.add(dev);
+
+        }
+    }
+    
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {    
+    	public void onReceive(Context context, Intent intent) {
+    		String action = intent.getAction();              
+    		if (BluetoothDevice.ACTION_FOUND.equals(action)) {            
+    			// Get the BluetoothDevice object from the Intent            
+    			BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);            
+    			// Add the device to an array adapter to show in a ListView            
+    			adapter.add(device);   
+    			onContentChanged();
+    			Log.i(tag, "gevonden device " + device.getName());
+    		}
+    	}
+    };
+
+	
+
+	@Override
+	public void onListItemClick(ListView l, View arg1, int arg2, long arg3) {
+        btadapter.cancelDiscovery();
+		BluetoothDevice device = (BluetoothDevice)adapter.getItem(arg2);
+		Log.i(tag, device.getName());
+        Intent myIntent = new Intent();
+        myIntent.putExtra("theDevice", device);
+        setResult(RESULT_OK, myIntent);
+        finish();
+    }
+	
+
+	
+
+}
---
+++ BluetoothRemote-master/app/src/main/java/nl/hu/zrb/btclient/ControlActivity.java
@@ -0,0 +1,234 @@
+package nl.hu.zrb.btclient;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+
+public class ControlActivity extends Activity {
+
+    int CONNECT_BT = 2351;
+    private Wachtrij<String> messageQueue;
+    ImageButton btnUp, btnDown, btnLeft, btnRight, btnFire;
+    TextView tv;
+    String tag = "ControlActivity";
+    ThreadThatSendsCommandsByBT t;
+    MenuItem connectItem;
+    RobotConnection myRobot = new RobotConnection();
+    boolean auto = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control);
+        tv = (TextView) findViewById(R.id.textView);
+        messageQueue = new Wachtrij<String>();
+
+        btnUp = (ImageButton) findViewById(R.id.imageButtonUp);
+        btnUp.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == android.view.MotionEvent.ACTION_DOWN) {
+                    messageQueue.voegToe("UP");
+                } else if (event.getAction() == android.view.MotionEvent.ACTION_UP) {
+                    messageQueue.voegToe("NULL");
+                }
+                return false;
+            }
+        });
+
+        btnDown = (ImageButton) findViewById(R.id.imageButtonDown);
+        btnDown.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == android.view.MotionEvent.ACTION_DOWN) {
+                    messageQueue.voegToe("DOWN");
+                } else if (event.getAction() == android.view.MotionEvent.ACTION_UP) {
+                    messageQueue.voegToe("NULL");
+                }
+                return false;
+            }
+        });
+
+
+        btnLeft = (ImageButton) findViewById(R.id.imageButtonLeft);
+        btnLeft.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == android.view.MotionEvent.ACTION_DOWN) {
+                    messageQueue.voegToe("LEFT");
+                } else if (event.getAction() == android.view.MotionEvent.ACTION_UP) {
+                    messageQueue.voegToe("NULL");
+                }
+                return false;
+            }
+        });
+
+
+        btnRight = (ImageButton) findViewById(R.id.imageButtonRight);
+        btnRight.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == android.view.MotionEvent.ACTION_DOWN) {
+                    messageQueue.voegToe("RIGHT");
+                } else if (event.getAction() == android.view.MotionEvent.ACTION_UP) {
+                    messageQueue.voegToe("NULL");
+                }
+                return false;
+            }
+        });
+
+        btnFire = (ImageButton) findViewById(R.id.imageButtonFire);
+        btnFire.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == android.view.MotionEvent.ACTION_DOWN) {
+                    messageQueue.voegToe("FIRE");
+                } else if (event.getAction() == android.view.MotionEvent.ACTION_UP) {
+                    messageQueue.voegToe("NULL");
+                }
+                return false;
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_control, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.connect) {
+            Intent intent = new Intent(this, ConnectActivity.class);
+            startActivityForResult(intent, CONNECT_BT);
+            item.setEnabled(false);
+            connectItem = item;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (resultCode == RESULT_OK && requestCode == CONNECT_BT) {
+            BluetoothDevice device = data.getParcelableExtra("theDevice");
+            new ConnectTask().execute(device);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        myRobot.closeConnection();
+        super.onDestroy();
+    }
+
+
+    private class ConnectTask extends AsyncTask<BluetoothDevice, Void, String> {
+
+
+        @Override
+        protected String doInBackground(BluetoothDevice... arg0) {
+            BluetoothDevice device = arg0[0];
+            try {
+
+                myRobot.connectToDevice(device);
+                t = new ThreadThatSendsCommandsByBT();
+                t.start();
+                return "connected";
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "error connecting";
+
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            tv.setText(result);
+            connectItem.setEnabled(true);
+        }
+
+    }
+
+
+    private class ThreadThatSendsCommandsByBT extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                String message = messageQueue.neemWeg();
+                Log.i(tag, message);
+                byte[] command = myRobot.createWriteCommand(message);
+
+                try {
+                    myRobot.sendCommand(command);
+
+                } catch (IOException e) {
+                    Log.i(tag, e.toString());
+                    Message m = new Message();
+                    m.getData().putString("theText", "Connection lost: " + e.getMessage());
+                    h.sendMessage(m);
+                    break;
+                }
+            }
+
+            myRobot.closeConnection();
+
+        }
+    }
+
+    private Handler h = new Handler() {
+        public void handleMessage(Message message) {
+            String s = (String) message.getData().get("theText");
+            tv.setText(s);
+            connectItem.setEnabled(true);
+        }
+    };
+
+
+    public void onAuto(View v) {
+        if (auto) {
+            messageQueue.voegToe("MANUAL");
+            auto = false;
+        } else {
+            messageQueue.voegToe("AUTO");
+            auto = true;
+        }
+    }
+
+    public void onB(View v) {
+        messageQueue.voegToe("B");
+    }
+
+    public void onCalibrate(View v) {
+        messageQueue.voegToe("CALIBRATE");
+    }
+
+
+}
